c9811d3e87190e0836a247810cd0d4f428906fb2
Nitin Kumar <78822174+kumar-nitin-tech@users.noreply.github.com>
2024-04-15 20:38:58 +0530

    Fractional Knapsack

--- New Start/Greedy Algo/FractionalKnapsack.cpp
+++ New Start/Greedy Algo/FractionalKnapsack.cpp
@@ -14,17 +14,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//Optimal Approach
-//TC:
-//SC:
-double fractionalKnapsack(vector<pair<int,int>> &items, int n, int w){
+struct item{
+    int value;
+    int weight;
+};
 
+static bool cmp(pair<double,item> a, pair<double,item> b){
+    return a.first > b.first;
+}
+
+double fractionalKnapsack(int N, int W, vector<item>arr){
+    vector<pair<double, item>> indiviualArray;
+    for(int i=0;i<N;i++){
+        double indivualValue = 1.0*(arr[i].value) / arr[i].weight;
+        indiviualArray.push_back({indivualValue,arr[i]});
+    }
+
+    sort(indiviualArray.begin(), indiviualArray.end(), cmp);
+
+    double ans = 0;
+
+    for(int i=0;i<N;i++){
+        if(indiviualArray[i].second.weight > W){
+            ans += W*indiviualArray[i].first;
+            W = 0;
+        }
+        else{
+            ans += indiviualArray[i].second.value;
+            W -= indiviualArray[i].second.weight;
+        }
+    }
+    return ans;
 }
 
 int main(){
-    int n,w;
-    cin>>n>>w;
-    vector<pair<int,int>> items(n);
-    for(auto &i:items)  cin>>i.first>>i.second;
+    int N,W;
+    cin>>N>>W;
+    vector<item> arr(N);
+    for(auto &i:arr){
+        cin>>i.value>>i.weight;
+    }
+    cout<<fractionalKnapsack(N,W,arr);
     return 0;
 }
